ecdf4518f8a8519d9e17897677b3fc10c5192cb9
Sai Krishna <99529667+saikrishna7783@users.noreply.github.com>
2023-12-01 23:17:08 +0530

    Lab 1

---
+++ 1. LED Blinking.ino
@@ -0,0 +1,16 @@
+// Pin 13 has an LED connected on most Arduino boards
+int led = 13;
+
+void setup() {
+  // the setup routine runs once when you press reset
+  // initialize the digital pin as an output.
+  pinMode(led, OUTPUT);
+}
+
+void loop() {
+  // the loop routine runs over and over again forever
+  digitalWrite(led, HIGH);  // turn the LED on (HIGH is the voltage level)
+  delay(1000);              // wait for a second
+  digitalWrite(led, LOW);   // turn the LED off by making the voltage LOW
+  delay(1000);              // wait for a second
+}
